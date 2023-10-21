fa642bd329663e41b6b6af363f2620185e48084f
raselsha <raselsha@gmail.com>
2023-10-21 21:37:22 +0600

    brand title update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="/">Shahadat Hossain</a>
+            <a class="navbar-brand h5" href="/">Shahadat Hossain</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
